9c5dfbb41afb116013353c272ee68249c26d3eb2
Andrey Avdeev <seorazer@gmail.com>
2020-01-06 03:51:42 +0300

    readme update & version 0.0.2 was published

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with io.open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
-version = "0.0.1"
+version = "0.0.2"
 
 setup(
     name="convolut_tensorboard",
@@ -21,7 +21,7 @@ setup(
     zip_safe=False,
     python_requires=">=3.7",
     install_requires=[
-        "convolut>=0.0.3",
+        "convolut>=0.0.4",
         "tensorboard>=2.1.0"
     ],
     keywords="convolut tensorboard",
